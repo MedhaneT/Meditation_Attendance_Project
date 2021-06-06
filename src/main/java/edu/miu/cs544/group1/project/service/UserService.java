d56b6db4dfbf82a9c0f27eeffc78f10162768cc4
misghinna <75956497+misghinna@users.noreply.github.com>
2021-06-06 10:07:59 -0500

    Update UserService.java

--- src/main/java/edu/miu/cs544/group1/project/service/UserService.java
+++ src/main/java/edu/miu/cs544/group1/project/service/UserService.java
@@ -4,17 +4,5 @@ import edu.miu.cs544.group1.project.controller.dto.RegisterUserDto;
 import edu.miu.cs544.group1.project.domain.User;
 
 public interface UserService {
-    // creat
-    // read
-    // update
-    // delete
-
-    List<Course> findAll();
-    Optional<Course> findById(Long id );
-    Optional<Course> findByCode(String name);
-    void createCource(Course course);
-    void removeCource(Long id);
-    
-    
-    //User registerUser(RegisterUserDto registerUserDto);
+    User registerUser(RegisterUserDto registerUserDto);
 }
