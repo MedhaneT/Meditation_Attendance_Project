70cb9744df22956b974a97a4676a4313e4263b36
tonynguyenit <tonynguyenit@outlook.com>
2021-06-09 11:22:56 -0500

    Merge attendance to 1 controller

--- src/main/java/edu/miu/cs544/group1/project/controller/AttendanceRecordController.java
+++ src/main/java/edu/miu/cs544/group1/project/controller/AttendanceRecordController.java
@@ -1,16 +1,19 @@
 package edu.miu.cs544.group1.project.controller;
 
+import com.google.zxing.ChecksumException;
+import com.google.zxing.FormatException;
+import com.google.zxing.NotFoundException;
 import edu.miu.cs544.group1.project.controller.dto.AttendanceRecordDto;
 import edu.miu.cs544.group1.project.domain.AttendanceRecord;
-import edu.miu.cs544.group1.project.domain.Course;
-import edu.miu.cs544.group1.project.repository.CourseRepository;
+import edu.miu.cs544.group1.project.domain.Location;
+import edu.miu.cs544.group1.project.repository.LocationRepository;
 import edu.miu.cs544.group1.project.service.AttendanceRecordService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
+import java.io.IOException;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
 @RequestMapping("/attendances")
@@ -18,6 +21,8 @@ public class AttendanceRecordController {
 
     @Autowired
     private AttendanceRecordService attendanceRecordService;
+    @Autowired
+    private LocationRepository locationRepository;
 
     @GetMapping
     public List<AttendanceRecord> findAll() {
@@ -27,15 +32,24 @@ public class AttendanceRecordController {
 
     @PostMapping
     public void create(@Valid @RequestBody AttendanceRecordDto attendanceRecordDto) {
-        attendanceRecordService.findByAttendanceScanTime(attendanceRecordDto.getScanTime()).ifPresent((v)->{
+        attendanceRecordService.findByAttendanceScanTime(attendanceRecordDto.getScanTime()).ifPresent((v) -> {
             throw new RuntimeException();
         });
         attendanceRecordService.createAttendanceRecord(attendanceRecordDto);
     }
 
     @DeleteMapping("/{id}")
-    public void deleteAttendanceRecord(@PathVariable long id){
+    public void deleteAttendanceRecord(@PathVariable long id) {
         attendanceRecordService.removeAttendanceRecord(id);
+    }
+
+    @PostMapping("/{barcodeLocation}/{barcodeString}")
+    public void barcodeAttedance(
+            @PathVariable(name = "barcodeLocation") String barcodeLocation,
+            @PathVariable(name = "barcodeString") String barcodeString) throws ChecksumException, NotFoundException, IOException, FormatException {
+        Location location = locationRepository.findByName(barcodeLocation)
+                .orElseThrow(() -> new RuntimeException("Location not found"));
+        attendanceRecordService.saveAttendance(barcodeString, location);
     }
 
 }
