c274a06b74407400f8f7c37a073054b991bb7bd3
tonynguyenit <tonynguyenit@outlook.com>
2021-06-10 10:05:26 -0500

    Add unique student_id and session_id on AttendanceRecode

--- src/main/java/edu/miu/cs544/group1/project/domain/AttendanceRecord.java
+++ src/main/java/edu/miu/cs544/group1/project/domain/AttendanceRecord.java
@@ -8,6 +8,10 @@ import java.time.LocalDateTime;
 
 @Entity
 @Data
+@Table(
+        uniqueConstraints=
+        @UniqueConstraint(columnNames={"student_id", "session_id"})
+)
 public class AttendanceRecord {
     @Id
     @GeneratedValue
